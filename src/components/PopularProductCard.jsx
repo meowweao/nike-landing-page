4b2fad72a55233a603f89412b2469961226a6ba0
meowweao <ganeshfalak01@gmail.com>
2023-12-18 23:11:18 +0530

    Style Popular Products Section

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,20 @@
+import { star } from "../assets/icons";
+
+
+const PopularProductCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-col flex-1 w-full max-sm:w-full">
+        <img src={ imgURL } className="w-[280px] h-[280px]"  alt={ name } />
+
+        <div className="mt-8 flex justify-start gap-2.5">
+            <img src={star} alt="rating" width={24} height={24 }/>
+            <p className="font-montserrat leading-normal text-xl text-slate-gray">(4.5)</p>
+        </div>
+
+        <h3 className="mt-2 font-semibold font-palanquin leading-normal text-2xl">{ name }</h3>
+        <p className="mt-2 font-semibold font-montserrat text-coral-red leading-normal">{ price }</p>
+    </div>
+  )
+}
+
+export default PopularProductCard
