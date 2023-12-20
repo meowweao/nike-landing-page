bc3e409d13414193e0937717891f6750d8bc29b8
meowweao <ganeshfalak01@gmail.com>
2023-12-20 23:53:07 +0530

    Style SuperQuality Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ label, iconURL }) => {
     <button className="flex justify-center items-center gap-2 px-7 py-5 font-montserrat text-lg leading-none bg-coral-red rounded-full border-coral-red text-white-400">
         {label}
 
-        <img src={iconURL} alt="right arrow icon" className="ml-2 rounded-full w-5 h-5" />
+        {iconURL && <img src={iconURL} alt="right arrow icon" className="ml-2 rounded-full w-5 h-5" />}
     </button>
   )
 }
